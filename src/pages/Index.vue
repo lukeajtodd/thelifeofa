ac0cce52b55dc079e29bf57100acc87db34b660b
Luke Todd <luketodd@zoho.com>
2019-04-26 20:22:34 +0100

    Setting up some workspace settings and testing page query

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,28 @@
 <template>
   <Layout>
-    
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    <p v-for="(post, i) of $page.posts" :key="i">
+      {{ post.title }}
     </p>
-
   </Layout>
 </template>
+
+<page-query>
+{
+  posts {
+    title
+  }
+}
+</page-query>
 
 <script>
 export default {
   metaInfo: {
     title: 'Hello, world!'
   }
-}
+};
 </script>
 
 <style>
